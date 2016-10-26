4fd95527147cd095182401ab3d076b0d305bd54b
fre14 <femile@blueyonder.co.uk>
2016-10-26 11:27:17 +0100

    Rename field names

--- src/rover.rb
+++ src/rover.rb
@@ -4,9 +4,9 @@ class Rover
   attr_reader :positionX
 	attr_reader :positionY
 
-	def initialize(direction, postionX, positionY)
-		@postionX = postionX
-		@postionY = positionY
+	def initialize(direction, positionX, positionY)
+		@positionX = positionX
+		@positionY = positionY
 	  @direction = direction
 	end
 
--- tests/test_rover.rb
+++ tests/test_rover.rb
@@ -6,6 +6,8 @@ class TestRover < Test::Unit::TestCase
   def test_initialise
   	rover = Rover.new('N', 0, 0)
   	assert_equal(rover.direction, 'N')
+    assert_equal(rover.positionX, 0)
+    assert_equal(rover.positionY, 0)
   end
 
 end
