c39c8c7404339d656ce4b895f9363bd910ab1ee5
fre14 <femile@blueyonder.co.uk>
2016-12-04 18:32:27 +0000

    Initialise Map with an array that will hold out of bound coords

--- src/map.rb
+++ src/map.rb
@@ -2,12 +2,13 @@ class Map
 
   attr_reader :width
   attr_reader :height
+  attr_reader :out_of_bound_coordinates
 
 	def initialize(width, height)
 		@width = width
 		@height = height
+		@out_of_bound_coordinates = []
 	end
 
-  # TODO: Add obstacle function that creates obstacles in the map
 
 end
