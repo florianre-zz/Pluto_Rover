24b4379f71f94179b7ffe408c4845342cfc2144c
fre14 <femile@blueyonder.co.uk>
2016-12-04 19:34:34 +0000

    Add map tests for scent feature

--- tests/test_map.rb
+++ tests/test_map.rb
@@ -9,4 +9,29 @@ class TestMap < Test::Unit::TestCase
     assert_equal(map.height, 10)
   end
 
+  def test_leave_scent
+    width = 10
+    height = 10
+    map = Map.new(width, height)
+
+    map.leave_scent(width - 1, height - 1)
+    map.leave_scent(width - width, 4)
+
+    assert_equal(map.out_of_bound_coordinates.length, 2)
+    assert_true(map.out_of_bound_coordinates.include?([width - 1, height - 1]))
+    assert_true(map.out_of_bound_coordinates.include?([width - width, 4]))
+  end
+
+  def test_has_scent
+    width = 10
+    height = 10
+    map = Map.new(width, height)
+
+    map.leave_scent(width - 1, height - 1)
+    map.leave_scent(width - width, 4)
+
+    assert_true(map.has_scent(width - 1, height - 1))
+    assert_true(map.has_scent(width - width, 4))
+  end
+
 end
