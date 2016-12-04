4f8b050d2820eb5d18e10dd97000729e87b2874e
Florian Emile <florian.emile14@gmail.com>
2016-12-04 21:22:24 +0000

    Update test_map.rb

--- tests/test_map.rb
+++ tests/test_map.rb
@@ -4,8 +4,8 @@ require './src/map.rb'
 class TestMap < Test::Unit::TestCase
 
   def test_initialise
-  	map = Map.new(10, 10)
-  	assert_equal(map.width, 10)
+    map = Map.new(10, 10)
+    assert_equal(map.width, 10)
     assert_equal(map.height, 10)
   end
 
